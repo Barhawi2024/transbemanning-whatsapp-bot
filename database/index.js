f07e193c7f201079311079c940bfd9e52d278c58
Barhawi2024 <hasan.barhawi@icloud.com>
2026-07-12 22:35:07 +0200

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,8 +1,28 @@
 const { pool, query } = require('./connection');
 const setupDatabase = require('./setup');
 
+const drivers = require('./drivers');
+const activities = require('./activities');
+const messages = require('./messages');
+const workSessions = require('./workSessions');
+const commands = require('./commands');
+const admins = require('./admins');
+const gps = require('./gps');
+const reports = require('./reports');
+const pdfReports = require('./pdfReports');
+
 module.exports = {
   pool,
   query,
-  setupDatabase
+  setupDatabase,
+
+  ...drivers,
+  ...activities,
+  ...messages,
+  ...workSessions,
+  ...commands,
+  ...admins,
+  ...gps,
+  ...reports,
+  ...pdfReports
 };
