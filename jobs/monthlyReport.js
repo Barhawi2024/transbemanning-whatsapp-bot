4b4202becb89143fa7c371e4d6e1ad6fc2a94ea2
Barhawi2024 <hasan.barhawi@icloud.com>
2026-07-15 18:37:56 +0200

    Update monthlyReport.js

--- jobs/monthlyReport.js
+++ jobs/monthlyReport.js
@@ -1,1 +1,13 @@
+const cron = require("node-cron");
 
+function startMonthlyReportJob() {
+    cron.schedule("5 0 1 * *", async () => {
+        console.log("📊 Startar automatisk månadsrapport...");
+    }, {
+        timezone: "Europe/Stockholm"
+    });
+}
+
+module.exports = {
+    startMonthlyReportJob
+};
