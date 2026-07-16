84b773cdd9d2fdd15da9f5ba0aacb780f9c65971
Barhawi2024 <hasan.barhawi@icloud.com>
2026-07-16 08:10:44 +0200

    Update index.js

--- database/index.js
+++ database/index.js
@@ -11,7 +11,7 @@ const gps = require('./gps');
 const reports = require('./reports');
 const pdfReports = require('./pdfReports');
 const pendingActions = require('./pendingActions');
-
+const workSessions = require('./workSessions');
 module.exports = {
   pool,
   query,
@@ -27,4 +27,6 @@ module.exports = {
   ...reports,
   ...pdfReports,
   ...pendingActions
+  ...workSessions,
+  
 };
